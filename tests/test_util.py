68cd69d835bc96e294363ea39fee644eccdb481d
slaily <slavov.iliyan96@gmail.com>
2020-10-28 19:22:08 +0200

    Add test case for reading a message from the IO stream

--- tests/test_util.py
+++ tests/test_util.py
@@ -3,13 +3,20 @@ import asyncio
 from unittest import TestCase
 from unittest.mock import patch
 
-import util
+from aiotaxi import util
 
 
 class UtilTestCase(TestCase):
-    @patch('util.write_message')
+    @patch('aiotaxi.util.write_message')
     def test_write_message(self, mock_writer):
         mock_writer.return_value = asyncio.StreamWriter
         writer = asyncio.run(util.write_message())
 
         self.assertIs(writer, asyncio.StreamWriter)
+
+    @patch('aiotaxi.util.read_message')
+    def test_read_message(self, mock_reader):
+        mock_reader.return_value = asyncio.StreamReader
+        reader = asyncio.run(util.read_message())
+
+        self.assertIs(reader, asyncio.StreamReader)
