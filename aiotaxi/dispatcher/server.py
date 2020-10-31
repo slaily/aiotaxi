fa219f6d11c637d1f1325365c29da1f29baee104
slaily <slavov.iliyan96@gmail.com>
2020-10-31 18:45:20 +0200

    Add handling dispatching message pattern in the Dispatcher server

--- aiotaxi/dispatcher/server.py
+++ aiotaxi/dispatcher/server.py
@@ -1,17 +1,20 @@
 import asyncio
 
-
 from aiotaxi import common
+
+from . import utils
 
 
 async def handle_client(reader, writer):
     addr = writer.get_extra_info('peername')
     common.display_connected_client(addr)
-    message = await common.read_message(reader)
-    common.display_received_message(message, addr)
+    received_message = await common.read_message(reader)
+    common.display_received_message(received_message, addr)
 
-    if message.lower().startswith('dispatcher'):
-        pass
+    if received_message.lower().startswith('dispatcher'):
+        dispatcher_id = utils.assign_dispatcher('Available')
+        message_to_send = dispatcher_id.encode('utf-8')
+        await common.write_message(writer, message_to_send)
 
     await common.close_stream_writer(writer)
     print(f'Client {addr!r} - connection closed.')
