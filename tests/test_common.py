f4c90de448f7dc48978a06debd38f7bbf2414751
slaily <slavov.iliyan96@gmail.com>
2020-10-31 13:02:50 +0200

    Rename and refactor the test file

--- tests/test_common.py
+++ tests/test_common.py
@@ -3,20 +3,20 @@ import asyncio
 from unittest import TestCase
 from unittest.mock import patch
 
-from aiotaxi import util
+from aiotaxi import common
 
 
-class UtilTestCase(TestCase):
-    @patch('aiotaxi.util.write_message')
+class CommonTestCase(TestCase):
+    @patch('aiotaxi.common.write_message')
     def test_write_message(self, mock_writer):
         mock_writer.return_value = asyncio.StreamWriter
-        writer = asyncio.run(util.write_message())
+        writer = asyncio.run(common.write_message())
 
         self.assertIs(writer, asyncio.StreamWriter)
 
-    @patch('aiotaxi.util.read_message')
+    @patch('aiotaxi.common.read_message')
     def test_read_message(self, mock_reader):
         mock_reader.return_value = asyncio.StreamReader
-        reader = asyncio.run(util.read_message())
+        reader = asyncio.run(common.read_message())
 
         self.assertIs(reader, asyncio.StreamReader)
