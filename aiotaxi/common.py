c7a4081a637e8a7218a4b40b2d6b3a5d6432816e
slaily <slavov.iliyan96@gmail.com>
2020-11-01 10:38:01 +0200

    Add an utility functionality that returns the connected client address

--- aiotaxi/common.py
+++ aiotaxi/common.py
@@ -22,3 +22,10 @@ def display_received_message(message, addr):
 async def close_stream_writer(writer):
     writer.close()
     await writer.wait_closed()
+
+
+def get_client_addr(writer):
+    addr = writer.get_extra_info('peername')
+    client_host, client_port = addr
+
+    return ':'.join([client_host, str(client_port)])
