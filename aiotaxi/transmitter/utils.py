201400fae072b6926caa73d060f339866a42a795
slaily <slavov.iliyan96@gmail.com>
2020-11-02 20:22:50 +0200

    Add utility function for transmitting message to Dispatcher server

--- aiotaxi/transmitter/utils.py
+++ aiotaxi/transmitter/utils.py
@@ -2,6 +2,9 @@ import asyncio
 
 from json import dumps
 
+from aiotaxi import common
+from aiotaxi import settings
+
 
 async def establish_ext_serv_conn(host, port):
     reader = None
@@ -26,3 +29,27 @@ def format_message_to_send(*args):
     }
 
     return dumps(msg_to_send_dict).encode()
+
+
+async def transmit_message_to_dispatcher(from_addr, message):
+    reader, writer = await establish_ext_serv_conn(
+        settings.DISPATCHER_HOST, settings.DISPATCHER_PORT
+    )
+
+    if not writer:
+        return False, b"Sorry, your request cannot be processed at this time!\n"
+
+    message_to_send =format_message_to_send(from_addr, message)
+    asyncio.create_task(
+        common.write_message(writer, message_to_send)
+    )
+
+    if not reader:
+        return False, b"Sorry, your request cannot be processed at this time!\n"
+
+    received_message = await common.read_message(reader)
+    dispatcher_addr = common.get_client_addr(writer)
+    common.display_received_message(received_message, dispatcher_addr)
+    asyncio.create_task(common.close_stream_writer(writer))
+
+    return True, received_message.encode()
