595e7459af98caf51b5dd9e1879ea608fb9c6d48
slaily <slavov.iliyan96@gmail.com>
2020-11-02 20:16:26 +0200

    Create transmitter utility function for formatting message before send

--- aiotaxi/transmitter/utils.py
+++ aiotaxi/transmitter/utils.py
@@ -1,5 +1,7 @@
 import asyncio
 
+from json import dumps
+
 
 async def establish_ext_serv_conn(host, port):
     reader = None
@@ -14,3 +16,13 @@ async def establish_ext_serv_conn(host, port):
         print(str(exc))
 
     return reader, writer
+
+
+def format_message_to_send(*args):
+    addr, message = args
+    msg_to_send_dict = {
+        'from_addr': addr,
+        'message': message
+    }
+
+    return dumps(msg_to_send_dict).encode()
